764aafd8452dc3fdc8c1627ed12a525da89ee6f3
turupawn <ahmed.hn.43@gmail.com>
2021-07-14 18:01:25 -0600

    stuff

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraKey = "a3e70735b4.....";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
-  contracts_build_directory: "./client/contracts",
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
     },
+    rinkeby: {
+      provider: function() { 
+       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/a3e70735b4cf401b9148e1fea8f5a288");
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+  },
+  mocha: {
   },
   compilers: {
     solc: {
-      version: "0.8.5",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.5",
     }
   },
   db: {
